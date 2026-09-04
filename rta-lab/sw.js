00117d07332f6a372a27dd164e0351f7e61f32e9
andreiclim77-cell <andrei.clim77@gmail.com>
2026-09-04 09:25:55 +0300

    Refresh Lab cache after Clapton triangulation sync

--- rta-lab/sw.js
+++ rta-lab/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "rta-lab-v10-five-wire-clapton-620-personal-5wrap-tc-sixwrap-claptonprefs-asylumwatt-master-20260903";
+const CACHE = "rta-lab-v10-five-wire-clapton-620-personal-5wrap-tc-sixwrap-fourpairs-no29spaced-20260904";
 const FALLBACK = "./index.html?v=10";
 const ASSETS = [
   "./index.html?v=10",
